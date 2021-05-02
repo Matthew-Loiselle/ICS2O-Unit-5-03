0b758d2a44505cc4cfc1a4685f8ab9b370682e95
MatthewLoiselle <>
2021-05-02 06:30:18 +0000

    errors

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.getElementById('firstbutton').addEventListener('click', DisplayMessage)
 
-
-
 function DisplayMessage () {
- alert(document.getElementById('input').value)
+  alert(document.getElementById('input').value)
 }
